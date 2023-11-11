d5ce56dc5a885f703d831274e7e59822b78ee36c
tjbui <143781210+tjbui@users.noreply.github.com>
2023-11-11 15:33:14 -0500

    Create StoreUnitTest.java

---
+++ src/StoreUnitTest.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class StoreUnitTest {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(1);
+        Store store = new Store(arr, "Target", "target@gmail.com", 12345);
+        System.out.println(store.getName().equals("Target"));
+        System.out.println(store.getSellerEmail().equals("target@gmail.com"));
+        System.out.println(store.getId() == 12345);
+        System.out.println(store.getProducts() == arr);
+        store.setId(54321);
+        System.out.println(store.getId() == 54321);
+        store.setName("Walmart");
+        System.out.println(store.getName().equals("Walmart"));
+        ArrayList<Integer> arr2 = new ArrayList<Integer>();
+        arr2.add(2);
+        store.setProducts(arr2);
+        System.out.println(store.getProducts() == arr2);
+        store.setSellerEmail("walmart@gmail.com");
+        System.out.println(store.getSellerEmail().equals("walmart@gmail.com"));
+
+        System.out.println(store.toStringFormat().equals("Walmart,walmart@gmail.com,54321,2"));
+        System.out.println(Store.fromStringFormat("Walmart,walmart@gmail.com,54321,2").getName().equals("Walmart"));
+        System.out.println(Store.fromStringFormat("Walmart,walmart@gmail.com,54321,2").getId() == 54321);
+        System.out.println(Store.fromStringFormat("Walmart,walmart@gmail.com,54321,2").getSellerEmail().equals("walmart@gmail.com"));
+        System.out.println(Store.fromStringFormat("Walmart,walmart@gmail.com,54321,2").getProducts().get(0) == 2);
+        System.out.println(Store.fromStringFormat("Walmart,walmart@gmail.com,54321,2").toStringFormat().equals("Walmart,walmart@gmail.com,54321,2"));
+
+    }
+}
