c9f37a9d8d2793ba6c1a0b52aab4de7e541e9b1e
Seth Hartzler <73349588+SWBuxler@users.noreply.github.com>
2023-11-13 17:51:35 -0500

    Last debug (hopefully)

--- src/DataManager.java
+++ src/DataManager.java
@@ -46,7 +46,7 @@ public class DataManager {
             this.dummyProduct = new Product("Product not found",
                     "Product description not found", -1, 0, 0.0, -1);
         } catch (Exception e) {
-            System.out.println("File not found")
+            System.out.println("File not found");
         }
         this.dummyUser = new User("User not found", "");
         this.dummyStore = new Store(new ArrayList<Integer>(), "Store not found",
