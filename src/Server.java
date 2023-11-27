1221e6c074eb7c4238b7f02dcb03b854b86ff2db
Seth Hartzler <73349588+SWBuxler@users.noreply.github.com>
2023-11-27 13:39:34 -0500

    Added DataManager to Server

--- src/Server.java
+++ src/Server.java
@@ -2,23 +2,20 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server implements Runnable {
-    public Server() {
-        //?
-    }
-    @Override
-    public void run() {
+public class Server {
+    public static void main(String[] args) {
         try {
             // Initialize your server socket here, e.g., ServerSocket serverSocket = new ServerSocket(port);
             int port = 12345;
 
             ServerSocket serverSocket = new ServerSocket(port);
+            DataManager dm = new DataManager();
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
 
                 // Create a ServerThread for each client connection
-                ServerThread serverThread = new ServerThread(clientSocket, Main3.dataManager);
+                ServerThread serverThread = new ServerThread(clientSocket, dm);
                 serverThread.start();
             }
         } catch (IOException e) {
